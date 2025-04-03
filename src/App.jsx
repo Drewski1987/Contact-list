429c97e07a6958c0f748e662c46f81b46d5fa2a1
Maurice Drew <drewm1987@gmail.com>
2025-04-03 15:45:48 -0500

    finishing touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
-
 import './App.css'
+import './Components/ContactList.jsx'
+import ContactList from './components/ContactList'
+import ContactRow from './Components/ContactRow'
+import SelectedContact from './components/SelectedContact';
 
-function App() {
+
+function App(){
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+
+
+
   
   return (
     <>
+      {selectedContactId ? (
+        <SelectedContact selectedContactId = {selectedContactId} setSelectedContactId = {setSelectedContactId}/>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      ) }
       
     </>
-  )
+  );
 }
+
 
 export default App
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,43 @@
+
+import { useState } from "react";
+import ContactRow from "./ContactRow";
+import { useEffect } from "react";
 import React from "react";
 
-export default function ContactList(){
+
+const dummyContacts = [
+    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+  ];
+
+
+
+ function ContactList({setSelectedContactId}){
+    
+    const [contacts, setContacts] = useState(dummyContacts);
+
+    console.log(("state", contacts))
+  
+
+    useEffect (()=> {
+        async function fetchContacts(){
+            try {
+            const res = await fetch(" https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users ");
+            const data = await res.json();
+
+            console.log(data)
+            setContacts(data)
+
+            }catch (error) {
+                console.error(error);
+            }
+        }
+        fetchContacts()
+        
+         },[]);
+
+         console.log("Here is the updated state:", contacts)
 
     return
     ( 
@@ -17,9 +54,12 @@ export default function ContactList(){
               <td>Phone</td>
             </tr>
             {
-               // Map over data here
+               contacts.map((contact)=> {
+                return <ContactRow key={contact.id} contact={contact} />;
+               }) 
              }
           </tbody>
         </table>
     ); 
 }
+    export default ContactList
